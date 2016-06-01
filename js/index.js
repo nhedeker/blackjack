efc993094f9763396729f19d789b116c498380fd
Natasha Hedeker <Natasha.Hedeker@gmail.com>
2016-06-01 12:34:17 -0700

    added code

--- js/index.js
+++ js/index.js
@@ -38,19 +38,33 @@
       $('.navbar-fixed').after('<div id="board" class="center-align"></div');
       var $board = $('#board');
 
+      var updateTotal = function(person, amount) {
+        person.total = amount;
+        displayTotal(person);
+      }
+
+      var displayTotal = function(person) {
+        if (person === dealer){
+          $('#dealer-total').text(person.total);
+        }
+        if (person === player){
+          $('#player-total').text(person.total);
+        }
+      };
+
       var displayGame = function() {
         var $row1 = $('<div class="row" id="row-1"></div>');
-        $row1.append('<div class="col s2"></div>');
+        $row1.append('<div class="col s2 total valign-wrapper"><h2>Dealer Total</h2><h2 id="dealer-total"></h2></div>');
         $row1.append('<div class="col s8 valign-wrapper" id="dealerHand"></div>');
-        $row1.append('<div class="col s2"></div>');
+        $row1.append('<div class="col s2 valign-wrapper" id="state-buttons"><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="play-again">Play Again</button><p></p><button data-target="modal1" id="rules" class="btn-large modal-trigger yellow-text purple darken-2">Rules</button></div>');
 
         var $row3 = $('<div class="row" id="row-3"></div>');
 
-        $row3.append('<div class="col s2 valign-wrapper" id="blackjack-buttons"><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="hit">Hit</button><p></p><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="stand">Stand</button></div>');
+        $row3.append('<div class="col s2 valign-wrapper" id="blackjack-buttons"><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="hit">Hit</button><p></p><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="stand">Stand</button><p></p><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="adj-bet">Adjust Bet</button></div>');
 
         $row3.append('<div class="col s8 valign-wrapper" id="playerHand"></div>');
 
-        $row3.append('<div class="col s2 valign-wrapper" id="state-buttons"><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="play-again">Play Again</button><p></p><button class="btn-large z-depth-1 yellow-text purple darken-2 center-align" id="adj-bet">Adjust Bet</button><p></p><button data-target="modal1" id="rules" class="btn-large modal-trigger yellow-text purple darken-2">Rules</button></div>');
+        $row3.append('<div class="col s2 total valign-wrapper"><h2>Player Total</h2><h2 id="player-total"></h2></div>');
 
         $board.append($row1);
         $board.append($row3);
@@ -144,6 +158,9 @@
           player.hand.push(data3[0].cards[0]);
           dealer.hand.push(data4[0].cards[0]);
           displayHands();
+          updateTotal(player, calculateHand(player));
+          updateTotal(dealer, calculateHand(dealer));
+          playerTurn();
         });
       };
 
@@ -166,8 +183,7 @@
       };
 
       var displayNewCard = function(person) {
-        var imageURL;
-        imageURL = person.hand[person.hand.length-1].image;
+        var imageURL = person.hand[person.hand.length-1].image;
         var $card = $(`<img src=${imageURL}>`);
         if (person === dealer){
           $('#dealerHand').append($card);
@@ -181,7 +197,6 @@
         var total = 0;
         console.log(person);
         console.log(person.hand); //returns empty []
-        console.log(person[hand]); //returns hand is not defined
         for (var card of person.hand) {
           console.log('started counting loop');
           total += card.value;
@@ -193,7 +208,7 @@
       };
 
       var changeAce = function(person) {
-        for (card of person.hand) {
+        for (var card of person.hand) {
           if (card.value === 11) {
             card.value = 1;
             person.hasAce = false;
@@ -204,15 +219,15 @@
       };
 
       var dealerTurn = function() {
-        var total = calculateHand(dealer);
+        updateTotal(dealer);
         console.log(`dealer total: ${total}`);
 
-        if (total === 21) {
+        if (dealer.total === 21) {
           player.hasBlackjack ? endGame('tie') : endGame('dealer');
 
           return;
         }
-        if (total > 21) {
+        if (dealer.total > 21) {
           if (dealer.hasAce) {
             changeAce(dealer);
             dealerTurn();
@@ -225,30 +240,24 @@
             return;
           }
         }
-        if (total <= 16) {
-          dealer.hand.push(draw()[0]);
-          displayNewCard('dealer');
-          dealerTurn();
+        if (dealer.total <= 16) {
+          hit(dealer);
+        }
+        if (dealer.total < 21 && dealer.total >= 17) {
+          calculateWinner();
+        }
+      };
 
+      var calculateWinner = function(){
+        if (player.total > dealer.total){
+          endGame('player');
           return;
         }
-        if (total < 21 && total >= 17) {
-          var playerTotal = calculateHand(player);
-
-          if (playerTotal > total) {
-            endGame('player');
-
-            return;
-          }
-          if (playerTotal < total) {
-            endGame('dealer');
-
-            return;
-          }
-          endGame('tie');
-
+        if (dealer.total > player.total){
+          endGame('dealer');
           return;
         }
+        endGame('tie');
       };
 
       var endPlayerTurn = function(){
@@ -258,15 +267,15 @@
       };
 
       var playerTurn = function() {
-        var total = calculateHand(player)
-        console.log(`player total: ${total}`);
+        updateTotal(player);
+        console.log(`player total: ${player.total}`);
         if (player.hasBlackjack) {
           console.log("PLAYER HAS BLACKJACK");
           endPlayerTurn();
 
           return;
         }
-        if (total > 21){
+        if (player.total > 21){
           if (player.hasAce) {
             changeAce(player);
             playerTurn();
@@ -280,16 +289,10 @@
             return;
           }
         }
-        $('#hit').on('click', function(){
+        $('#hit').off();
+        $('#hit').on('click', function(event){
           console.log('PLAYER HIT');
-          var $promise2 = $.when(draw());
-          $promise2.done(function (data1) {
-            console.log(data1);
-            player.hand.push(data1.cards[0]);
-            displayNewCard(player);
-            playerTurn();
-            return;
-          });
+          hit(player);
         });
         $('#stand').on('click', function(){
           console.log('PLAYER STOOD');
@@ -299,9 +302,24 @@
         });
       };
 
+      var hit = function(person) {
+        var $promise = $.when(draw());
+        $promise.done(function (data) {
+          console.log(data);
+          console.log(person);
+          person.hand.push(data.cards[0]);
+          displayNewCard(person);
+          if (person === player){
+            playerTurn();
+          }
+          if (person === dealer){
+            dealerTurn();
+          }
+        });
+      };
+
       var startGame = function() {
         deal();
-        playerTurn();
       };
 
       displayGame();
